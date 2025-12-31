c8b6ed04990b9b94a26f0a800c295c6a80987125
Albert Tian Chen <albert_chen@g.harvard.edu>
2025-12-30 21:59:22 -0500

    add to __init__

--- gunpowder/nodes/__init__.py
+++ gunpowder/nodes/__init__.py
@@ -44,6 +44,7 @@ from .snapshot import Snapshot
 from .specified_location import SpecifiedLocation
 from .squeeze import Squeeze
 from .stack import Stack
+from .tensorstore_source import TensorstoreSource
 from .unsqueeze import Unsqueeze
 from .upsample import UpSample
 from .zarr_source import ZarrSource
